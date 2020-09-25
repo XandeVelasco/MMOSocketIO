76598e4afe519c8927e75c8408dadf7bd501d709
Alexandre Velasco Alves <54082209+XandeVelasco@users.noreply.github.com>
2020-09-25 00:52:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io');
+var io = require('socket.io')(http);
 var shortid = require('shortid');
 
 var ClientLookUp = {};
